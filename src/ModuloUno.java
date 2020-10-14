4ab88834e1f50d8b6b942113a0c48229a40d0eea
mell2262 <mellgomez.2262@gmail.com>
2020-10-14 13:42:00 +0200

    Fase2

--- src/ModuloUno.java
+++ src/ModuloUno.java
@@ -2,15 +2,13 @@
 public class ModuloUno {
 
 	public static void main(String[] args) {
-		/*FASE 1
-		 Crear tres variables string y inicialitzales con los datos pertinentes 
-		(nombre, apellido1, cognom2).
-		 Crear tres variables int y inicialitzales con los datos pertinentes 
-		(día, mes, año).
-		 Muestra por pantalla las variables string concatenando-en este orden 
-		(apellido1 + cognom2, + nombre).
-		 Muestra por pantalla las variables int concatenando-con "/" 
-		entre cada una de ellas.
+		/*FASE 2
+		 Sabiendo que en 1948 es un año bisiesto:
+		 Cree una constante con el valor del año (1948).
+		 Cree una variable que guarde cada cuando hay un año bisiesto.
+		 Calcular cuantos años bisiestos hay entre 1948 y su año de nacimiento y 
+		almacene el valor resultante en una variable.
+		 Muestre por pantalla el resultado del cálculo.
 
 		*/
 		
@@ -24,6 +22,13 @@ public class ModuloUno {
 		
 		System.out.println(apellido1+" "+cognom2+" "+nombre);
 		System.out.println(dia+"/"+mes+"/"+año);
+		//--------------------------------------------------------------//
+		int BISIESTO=1948;
+		int contBisiesto=4;
+		
+		double result=Math.floor((año-BISIESTO)/contBisiesto);
+		System.out.println(result);
+		
 	}
 
 }
